6f1e640b74efd5030c5de7d325e40df965f3686a
diondevara <42793740+diondevara@users.noreply.github.com>
2020-04-09 13:03:22 +0700

    Add files via upload

---
+++ Assets/Dungeon/FlyCamera1.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyCamera1 : MonoBehaviour {
+	public float movementSpeed = 10.0f;
+	
+
+    void Start()
+    {
+        
+    }
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W))//maju
+        {
+            
+            transform.position += transform.forward * movementSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))//mundur
+        {
+            transform.position -= transform.forward * movementSpeed;
+        }
+	if (Input.GetKeyDown(KeyCode.A))//muter kiri
+			transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);
+		
+	if (Input.GetKeyDown(KeyCode.D))//muter kanan
+			transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+       
+        if (Input.GetKeyDown(KeyCode.E))//placeholder pindah lantai
+		{
+                if(transform.position.x<=12 && transform.position.x>=8)//cek posisi x
+                {
+                    if(transform.position.z<=222 && transform.position.z>=218)//cek posisi z
+                    {
+                        Application.LoadLevel("lt2");
+                    }
+                }
+        }
+    }
+}
