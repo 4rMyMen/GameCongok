a39a1620ac19712f99aa492661fabb70c6c67086
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2020-04-16 15:18:53 +0700

    Add Texture and Menu

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void ToInn(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void ToShop(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void ToWorldMap(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void BackToTown(){
+        SceneManager.LoadScene(1);
+    }
+}
