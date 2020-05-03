359cac725e9cdd72774347f73abf32c36798a62e
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2020-05-03 16:15:42 +0700

    Re-upload part 2

---
+++ Assets/Menu/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+
+    }
+
+    IEnumerator LoadAsync (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while(operation.isDone == false)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void ToInn(){
+        SceneManager.LoadScene(2);
+    }
+
+    public void ToShop(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void ToWorldMap(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void BackToTown(){
+        SceneManager.LoadScene(1);
+    }
+
+}
