d6d3ca6ba4943dc3c4618f636a69aa32758a9ddd
Ivan Indrakusuma <ivan.17051@mhs.its.ac.id>
2020-05-09 00:28:08 +0700

    Change to First Person + Dungeon Nav

---
+++ Assets/Scenes/Dungeon/Script/UIAppear.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIAppear : MonoBehaviour
+{
+    public GameObject customImage;
+    public int sceneIndex;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.SetActive(true);
+        }    
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }    
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.SetActive(false);
+        }
+    }
+}
